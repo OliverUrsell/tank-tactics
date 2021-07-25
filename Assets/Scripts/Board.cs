f4c1cde72740f683a3aaa4c82dc921f62b49ed07
w78728ou <oliver.ursell@student.manchester.ac.uk>
2021-07-25 13:08:34 +0100

    Board automatically creates background tiles

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public GameObject tilePrefab;
+
+    /// <summary>
+    /// Horizontal size of the board in number of tiles
+    /// </summary>
+    [Tooltip("Horiztonal size of the board in number of tiles")]
+    [Header("Board Size")]
+    [MinAttribute(1)]
+    [SerializeField]
+    private int boardSizeX = 10;
+
+    /// <summary>
+    /// Vertical size of the board in number of tiles
+    /// </summary>
+    [Tooltip("Vertical size of the board in number of tiles")]
+    [MinAttribute(1)]
+    [SerializeField]
+    private int boardSizeY = 10;
+
+    /// <summary>
+    /// The gap between tiles in unity units
+    /// </summary>
+    [MinAttribute(0)]
+    [SerializeField]
+    [Tooltip("The gap between tiles in Unity Units")]
+    private float tileGap = 0.1F;
+
+    /// <summary>
+    /// Stores the Tile objects that make up the board. [0] is the botom left, [x + y*boardSizeX] is one up from the bottom left 
+    /// </summary>
+    private List<Tile> tiles = new List<Tile>();
+
+
+    /// <summary>
+    /// Instantites and positions the tiles that make up the board, storing them in <see cref="Board.tiles"/>
+    /// </summary>
+    /// <remarks>
+    /// Called at Start
+    /// </remarks>
+    void ConstructMap()
+    {
+
+        Vector2 tileScale = tilePrefab.GetComponent<Transform>().localScale;
+
+        
+        for (
+            float y = -(boardSizeY / 2 * (tileScale.y + tileGap));
+            y < boardSizeY / 2 * tileScale.y;
+            y += tileScale.y + tileGap
+        )
+        {
+            for (
+                float x = -(boardSizeX / 2 * (tileScale.x + tileGap));
+                x < boardSizeX / 2 * tileScale.x;
+                x += tileScale.x + tileGap
+            )
+            {
+                tiles.Add(PlaceTile(x, y));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Instantiates a tile onto the screen
+    /// </summary>
+    /// <param name="x">The X position to put the tile in, in Unity Units</param>
+    /// <param name="y">The Y position to put the tile in, in Unity Units</param>
+    /// <returns><see cref="Tile"/> obect representing the placed tile</returns>
+    private Tile PlaceTile(float x, float y)
+    {
+        return Instantiate(tilePrefab, new Vector2(x, y), Quaternion.identity).GetComponent<Tile>();
+    }
+
+    /// <summary>
+    /// Get the <see cref="Tile"/> at a given position in the board from the bottom left
+    /// </summary>
+    /// <param name="x">The horizontal position to find from the left</param>
+    /// <param name="y">The vertical position to find from the bottom</param>
+    /// <returns><see cref="Tile"/> representing the tile at <paramref name="x"/>, <paramref name="y"/> from the bottom left</returns>
+    private Tile getTileAtPosition(int x, int y)
+    {
+        return tiles[x + y * boardSizeX];
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        ConstructMap();
+        getTileAtPosition(0, 0).gameObject.SetActive(false);
+        getTileAtPosition(0, 1).gameObject.SetActive(false);
+        getTileAtPosition(1, 0).gameObject.SetActive(false);
+    }
+}
